fd8025f43aff93668e0de23ffc357036575a3aec
caroline <gensac.caroline@gmail.com>
2023-04-10 18:59:17 +0200

    exos finis

---
+++ exo_08.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, comment tu te nomme jeune chenapan ?"
+print "> "
+user_name = gets.chomp
+puts "Salut, #{user_name} !"
---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, comment tu te nomme jeune chenapan ?"
+print "> "
+user_name = gets.chomp
+puts "et quel est ton prénom ?"
+user_surname = gets.chomp
+puts "Salut, #{user_surname+user_name} !"
